f996b528bf395fa074d9512642d5f6dc4d36ec82
jpoehler <jpoehler@comcast.net>
2016-06-14 21:15:26 -0500

    navbar changes

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 
-
 app.get('/*', function(req, res){
     var file = req.params[0] || 'index.html';
     res.sendFile(path.join(__dirname, '/public/', file));
